d2b6bb9492b9025d4b4e0ea13a0003e0309698bf
Khoa <ldkhoa.11402@gmail.com>
2022-12-15 23:07:01 +0700

    adjust user table in database

--- academy.sql
+++ academy.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2022 at 05:06 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -9,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `academy`
+-- Database: `academy`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -24,7 +33,7 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`IDCate`, `Name`) VALUES
@@ -37,7 +46,7 @@ INSERT INTO `category` (`IDCate`, `Name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `chapter`
+-- Table structure for table `chapter`
 --
 
 CREATE TABLE `chapter` (
@@ -47,7 +56,7 @@ CREATE TABLE `chapter` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `chapter`
+-- Dumping data for table `chapter`
 --
 
 INSERT INTO `chapter` (`IDCourse`, `IDChapter`, `Name`) VALUES
@@ -130,7 +139,7 @@ INSERT INTO `chapter` (`IDCourse`, `IDChapter`, `Name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `circulativevideo`
+-- Table structure for table `circulativevideo`
 --
 
 CREATE TABLE `circulativevideo` (
@@ -144,11 +153,11 @@ CREATE TABLE `circulativevideo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `circulativevideo`
+-- Dumping data for table `circulativevideo`
 --
 
 INSERT INTO `circulativevideo` (`IDCourse`, `IDChapter`, `No`, `Name`, `URL`, `Length`, `IsPreview`) VALUES
-(1, 1, 1, 'Audit and other assurance engagements', 'fLHejDNA_CM', 0, 0),
+(1, 1, 1, 'Audit and other assurance engagements', 'fLHejDNA_CM', 50, 0),
 (1, 1, 2, 'Statutory audit and regulation', 'Ay3d_mNPEno', 0, 0),
 (1, 1, 3, 'Corporate governance', '6Kc5X_JhbxQ', 0, 0),
 (1, 1, 4, 'Ethics', '95zXiCITbNM', 0, 0),
@@ -207,7 +216,7 @@ INSERT INTO `circulativevideo` (`IDCourse`, `IDChapter`, `No`, `Name`, `URL`, `L
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `courses`
+-- Table structure for table `courses`
 --
 
 CREATE TABLE `courses` (
@@ -227,11 +236,11 @@ CREATE TABLE `courses` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `courses`
+-- Dumping data for table `courses`
 --
 
 INSERT INTO `courses` (`ID`, `Name`, `IDCategory`, `Topic`, `Image`, `TinyDesc`, `FullDesc`, `CourseFee`, `IDDiscount`, `IDInstructor`, `IsCompleted`, `CreatedTime`, `Viewer`) VALUES
-(1, 'HTML CSS from Zero to Hero', 1, '1', '', 'Learn modern HTML5, CSS3 and web design by building a stunning website for your portfolio! Includes flexbox and CSS Grid', '<ul>\r\n    <li>Become a modern and confident HTML and CSS developer, no prior knowledge needed!</li>\r\n    <li>Design and build a stunning real-world project for your portfolio from scratch</li>\r\n    <li>Modern, semantic and accessible HTML5</li>\r\n    <li>Modern CSS (previous CSS3), including flexbox and CSS Grid for layout</li>\r\n    <li>Important CSS concepts such as the box model, positioning schemes, inheritance, solving selector conflicts, etc.</li>\r\n    <li>A web design framework with easy-to-use rules and guidelines to design eye-catching websites</li>\r\n    <li>How to plan, sketch, design, build, test, and optimize a professional website</li>\r\n    <li>How to make websites work on every possible mobile device (responsive design)</li>\r\n    <li>How to use common components and layout patterns for professional website design and development</li>\r\n    <li>Developer skills such as reading documentation, debugging, and using professional tools</li>\r\n    <li>How to find and use free design assets such as images, fonts, and icons</li>\r\n    <li>Practice your skills with 10+ challenges (solutions included)</li>\r\n</ul>', 500000, 0, 0, 0, '2022-12-10', 0),
+(1, 'HTML CSS from Zero to Hero', 1, '1', '', 'Learn modern HTML5, CSS3 and web design by building a stunning website for your portfolio! Includes flexbox and CSS Grid', '<ul>\r\n    <li>Become a modern and confident HTML and CSS developer, no prior knowledge needed!</li>\r\n    <li>Design and build a stunning real-world project for your portfolio from scratch</li>\r\n    <li>Modern, semantic and accessible HTML5</li>\r\n    <li>Modern CSS (previous CSS3), including flexbox and CSS Grid for layout</li>\r\n    <li>Important CSS concepts such as the box model, positioning schemes, inheritance, solving selector conflicts, etc.</li>\r\n    <li>A web design framework with easy-to-use rules and guidelines to design eye-catching websites</li>\r\n    <li>How to plan, sketch, design, build, test, and optimize a professional website</li>\r\n    <li>How to make websites work on every possible mobile device (responsive design)</li>\r\n    <li>How to use common components and layout patterns for professional website design and development</li>\r\n    <li>Developer skills such as reading documentation, debugging, and using professional tools</li>\r\n    <li>How to find and use free design assets such as images, fonts, and icons</li>\r\n    <li>Practice your skills with 10+ challenges (solutions included)</li>\r\n</ul>', 500000, 1, 0, 0, '2022-12-10', 50),
 (2, 'React Native - The Practical Guide [2023]', 1, '1', '', 'Use React Native and your React knowledge to build native iOS and Android Apps - incl. Push Notifications, Hooks, Redux', '<ul>\r\n    <li>Learn how to use ReactJS to build real native mobile apps for iOS and Android</li>\r\n    <li>Develop cross-platform (iOS and Android) mobile apps without knowing Swift, ObjectiveC or Java/ Android</li>\r\n    <li>Explore React Native basics and advanced features!</li>\r\n    <li>Learn how to use key mobile app features like Google maps or the device camera</li>\r\n    <li>Create real-world native apps using React Native</li>\r\n    <li>Make truly reusable components that look great</li>\r\n    <li>Understand the terminology and concepts of Redux</li>\r\n    <li>Prototype and deploy your own applications to the Apple and Google Play Stores</li>\r\n    <li>Get up to speed with React design principles and methodologies</li>\r\n    <li>Discover mobile design patterns used by experienced engineers</li>\r\n</ul>', 500000, 0, 0, 0, '2022-12-10', 0),
 (3, 'Complete C# Unity Game Developer 2D', 1, '2', '', 'Learn Unity in C# & Code Your First Five 2D Video Games for Web, Mac & PC. The Tutorials Cover Tilemap', '<ul>\r\n    <li>Learn C#, a powerful modern language, from scratch. No prior programming experience is necessary.</li>\r\n    <li>Become excellent at using the Unity game engine.</li>\r\n    <li>Build a solid foundation for game design and game development that will help you build your own games.</li>\r\n    <li>Learn how object oriented programming works in practice.</li>\r\n    <li>Create playable game projects - good for your portfolio, or just for your own sense of achievement.</li>\r\n    <li>Transfer your knowledge from this course to .NET, other languages, and more.</li>\r\n    <li>Develop highly transferable coding problem solving skills.</li>\r\n    <li>Be part of an amazing and supportive community of people similar to you.</li> \r\n</ul>', 1280000, 0, 0, 0, '2022-12-10', 0),
 (4, 'Visual Effects for Games in Unity - Beginner To In', 1, '2', '', 'By the end of this course you will have started your own portfolio as a Triple-A Visual Effects artist for Games.', '<ul>\r\n    <li>Learn how to create effects for games with Unity.</li>\r\n    <li>Fundamental concepts about Visual Effects.</li>\r\n    <li>Creating Textures, Meshes and Shaders for Game VFX.</li>\r\n    <li>How to structure AAA Visual Effects.</li>\r\n</ul>', 800000, 0, 0, 0, '2022-12-10', 0),
@@ -250,7 +259,7 @@ INSERT INTO `courses` (`ID`, `Name`, `IDCategory`, `Topic`, `Image`, `TinyDesc`,
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `discount`
+-- Table structure for table `discount`
 --
 
 CREATE TABLE `discount` (
@@ -259,7 +268,7 @@ CREATE TABLE `discount` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `discount`
+-- Dumping data for table `discount`
 --
 
 INSERT INTO `discount` (`ID`, `PercentDiscount`) VALUES
@@ -268,7 +277,7 @@ INSERT INTO `discount` (`ID`, `PercentDiscount`) VALUES
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `participate`
+-- Table structure for table `participate`
 --
 
 CREATE TABLE `participate` (
@@ -278,10 +287,18 @@ CREATE TABLE `participate` (
   `Rating` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `participate`
+--
+
+INSERT INTO `participate` (`IDCourse`, `IDStudent`, `Feedback`, `Rating`) VALUES
+(1, 1, 'Bài giảng rất hay', 2),
+(1, 4, 'Quá tệ', 2.3);
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `topic`
+-- Table structure for table `topic`
 --
 
 CREATE TABLE `topic` (
@@ -291,7 +308,7 @@ CREATE TABLE `topic` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `topic`
+-- Dumping data for table `topic`
 --
 
 INSERT INTO `topic` (`IDCate`, `IDTopic`, `Name`) VALUES
@@ -307,7 +324,7 @@ INSERT INTO `topic` (`IDCate`, `IDTopic`, `Name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -316,13 +333,26 @@ CREATE TABLE `user` (
   `Email` varchar(50) NOT NULL,
   `Password` text NOT NULL,
   `Bio` text NOT NULL,
-  `Role` int(11) NOT NULL
+  `Role` int(11) NOT NULL,
+  `OTP` int(11) NOT NULL,
+  `IDSocial` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='r';
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`IDUser`, `FullName`, `Email`, `Password`, `Bio`, `Role`, `OTP`, `IDSocial`) VALUES
+(1, 'Lê Đăng Khoa', 'ldkhoa.11402@gmail.com', '$2a$10$NcSTuFuXanh9F13WjjAvhefVN6DMKOo5UQuOhP4nvN98rky.LoOje', '', 2, 0, ''),
+(2, 'Brad Traversy', 'bradtraversy@gmail.com', '$2a$10$NcSTuFuXanh9F13WjjAvhefVN6DMKOo5UQuOhP4nvN98rky.LoOje', '', 1, 0, ''),
+(3, 'Administrator', 'admin@admin.com', '$2a$10$NcSTuFuXanh9F13WjjAvhefVN6DMKOo5UQuOhP4nvN98rky.LoOje', '', 0, 0, ''),
+(4, 'Nguyễn Như Phước', 'phuocnhu@gmail.com', '', '', 2, 0, ''),
+(19, 'Lê Đăng Khoa', 'ledangkhoa1142002@gmail.com', '$2b$05$.KrMSq9LM4Po1KRphM3YfesJQe5TKe2GHydI50ZI756/kG6viN3d6', '', 2, 0, '');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `watched`
+-- Table structure for table `watched`
 --
 
 CREATE TABLE `watched` (
@@ -335,7 +365,7 @@ CREATE TABLE `watched` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `watchhistory`
+-- Table structure for table `watchhistory`
 --
 
 CREATE TABLE `watchhistory` (
@@ -344,88 +374,92 @@ CREATE TABLE `watchhistory` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`IDCate`);
 
 --
--- Chỉ mục cho bảng `chapter`
+-- Indexes for table `chapter`
 --
 ALTER TABLE `chapter`
   ADD PRIMARY KEY (`IDCourse`,`IDChapter`);
 
 --
--- Chỉ mục cho bảng `circulativevideo`
+-- Indexes for table `circulativevideo`
 --
 ALTER TABLE `circulativevideo`
   ADD PRIMARY KEY (`IDCourse`,`IDChapter`,`No`);
 
 --
--- Chỉ mục cho bảng `courses`
+-- Indexes for table `courses`
 --
 ALTER TABLE `courses`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Chỉ mục cho bảng `discount`
+-- Indexes for table `discount`
 --
 ALTER TABLE `discount`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Chỉ mục cho bảng `participate`
+-- Indexes for table `participate`
 --
 ALTER TABLE `participate`
   ADD PRIMARY KEY (`IDCourse`,`IDStudent`);
 
 --
--- Chỉ mục cho bảng `topic`
+-- Indexes for table `topic`
 --
 ALTER TABLE `topic`
   ADD PRIMARY KEY (`IDCate`,`IDTopic`);
 
 --
--- Chỉ mục cho bảng `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`IDUser`);
 
 --
--- Chỉ mục cho bảng `watched`
+-- Indexes for table `watched`
 --
 ALTER TABLE `watched`
   ADD PRIMARY KEY (`IDStudent`,`IDCourse`,`IDChapter`,`No`);
 
 --
--- Chỉ mục cho bảng `watchhistory`
+-- Indexes for table `watchhistory`
 --
 ALTER TABLE `watchhistory`
   ADD PRIMARY KEY (`IDStudent`,`IDCourse`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `IDCate` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT cho bảng `courses`
+-- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT cho bảng `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `IDUser` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IDUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
